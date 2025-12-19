8e71bc79aed38d6c10592248a532d63dac9370ff
Hanyu Zhang <hanyuzhang251@gmail.com>
2025-12-18 20:26:05 -0800

    blind tune 2
    blind tune

--- main/src/autons.cpp
+++ main/src/autons.cpp
@@ -13,7 +13,7 @@ constexpr float MATCHLOAD_ENTER_SPEED_MIN = 15;
 constexpr uint32_t MATCHLOAD_DURATION = 2000;
 
 constexpr float LONG_GOAL_ENTER_SPEED_MIN = 110;
-constexpr float LONG_GOAL_ENTER_SPEED_MAX = 80;
+constexpr float LONG_GOAL_ENTER_SPEED_MAX = 70;
 constexpr uint32_t LONG_GOAL_DURATION = 1000;
 
 constexpr float LONG_GOAL_CROSS_MIN_SPEED = 80;
@@ -28,6 +28,23 @@ void long_goals(const bool first)
 
 #define pn (first?1:-1)
 #define aj(a, b) (first?a:b)
+
+#define chr_params chassis.getPose().theta,aj(90,-90),2,0.8f
+
+    auto c_pose = []()
+    {
+        return chassis.getPose();
+    };
+
+    auto conditional_heading_reset = [](const float heading, const float predicted_heading, const float tolerance, const float acceptance)
+    {
+        const float diff = std::abs(heading - predicted_heading);
+
+        if (diff > tolerance) return heading;
+
+        return heading + acceptance * (predicted_heading - heading);
+    };
+
     intake_set_state(INTAKE);
 
     // move to matchloader 1
@@ -42,7 +59,7 @@ void long_goals(const bool first)
     pros::delay(MATCHLOAD_DURATION);
 
     // cross to other side
-    chassis.moveToPoint(chassis.getPose().x + aj(5, -5), chassis.getPose().y, 600, {.forwards = false}, false);
+    chassis.moveToPoint(c_pose().x + aj(5, -5), c_pose().y, 600, {.forwards = false}, false);
     chassis.turnToHeading(aj(-135, 45), 600, {}, false);
     chassis.moveToPoint(pn* -23, pn* 60, 1000, {.forwards = false, .minSpeed = LONG_GOAL_CROSS_MIN_SPEED, .earlyExitRange = 4}, false);
     set_matchloader(false);
@@ -50,12 +67,12 @@ void long_goals(const bool first)
     // flush with wall
     chassis.turnToHeading(aj(0, 180), 800, {}, false);
     const float side_distance = static_cast<float>(find_dist(right_dist, right_dist_offset, 0, Dir::Right, 100));
-    chassis.setPose(pn* (WALL_COORD - side_distance), chassis.getPose().y, chassis.getPose().theta);
+    chassis.setPose(pn* (WALL_COORD - side_distance), c_pose().y, c_pose().theta);
 
     // move to long goal
     drive_until_distance(0, 119, 450, 1, 0, 850);
     chassis.turnToHeading(aj(90, -90), 600, {}, false);
-    chassis.moveToPoint(pn* 0, pn* 47, 700, {.forwards = false, .maxSpeed = LONG_GOAL_ENTER_SPEED_MAX, }, false);
+    chassis.moveToPoint(pn* 26, pn* (47 +1), 600, {.forwards = false, .maxSpeed = LONG_GOAL_ENTER_SPEED_MAX, }, false);
     // scoring
     intake_set_state(SCORE_HIGH);
     chassis.arcade(-LONG_GOAL_ENTER_SPEED_MIN, 0);
@@ -71,20 +88,55 @@ void long_goals(const bool first)
     chassis.arcade(MATCHLOAD_ENTER_SPEED_MIN, 0);
     // finish matchloading while running reset
 
+    chassis.setPose(c_pose().x, c_pose().y, conditional_heading_reset(chr_params));
     match_load_reset(MATCHLOAD_DURATION, 0, aj(1, 3));
 
     // move to long goal
-    chassis.moveToPoint(pn* 0, pn* 47, 1500, {.forwards = false, .maxSpeed = LONG_GOAL_ENTER_SPEED_MAX,}, true);
-    pros::delay(700);
+    chassis.moveToPoint(pn* 26, pn* (47 +1), 1000, {.forwards = false, .maxSpeed = LONG_GOAL_ENTER_SPEED_MAX,}, false);
     // scoring
     intake_set_state(SCORE_HIGH);
     chassis.arcade(-LONG_GOAL_ENTER_SPEED_MIN, 0);
     pros::delay(LONG_GOAL_DURATION);
 
     // reset pose at long goal
+    chassis.setPose(c_pose().x, c_pose().y, conditional_heading_reset(chr_params));
     match_load_reset(MATCHLOAD_DURATION, 0, aj(1, 3));
 
     set_matchloader(false);
+}
+
+void park_cross(const bool first)
+{
+#define pn (first?1:-1)
+#define aj(a, b) (first?a:b)
+    // position to park zone
+    intake_set_state(aj(IDLE, INTAKE));
+    chassis.moveToPose(pn* 63.5, pn* 20, aj(170, -10), 1500, {.horizontalDrift = 8, .minSpeed = 60, .earlyExitRange = 2}, false);
+    chassis.turnToHeading(aj(170, -10), 300, {}, false);
+    chassis.arcade(60, 0);
+    pros::delay(600);
+
+    // enter park zone
+    intake_set_state(INTAKE);
+    // sharp to initially enter
+    chassis.arcade(90, 0);
+    pros::delay(350);
+    // slow cross
+    chassis.arcade(70, 0);
+    const uint32_t cross_time = aj(3100,2000);
+    constexpr uint32_t drop_time = 900;
+    pros::delay(drop_time);
+    if (first) set_matchloader(true);
+    pros::delay(cross_time - drop_time);
+
+    if (!first) return;
+
+    // correct heading
+    chassis.turnToHeading(180, 600, {}, false);
+    // back up
+    chassis.arcade(-60, 0);
+    pros::delay(800);
+    chassis.arcade(0, 0);
 }
 
 void skills()
@@ -121,32 +173,7 @@ void skills()
 
     // 3. mid goal
 
-    // position to park zone
-    intake_set_state(IDLE);
-    chassis.moveToPose(63.5, 24, 170, 1500, {.horizontalDrift = 8, .minSpeed = 60, .earlyExitRange = 2}, false);
-    chassis.turnToHeading(170, 300, {}, false);
-    chassis.arcade(60, 0);
-    pros::delay(1000);
-
-    // enter park zone
-    intake_set_state(INTAKE);
-    // sharp to initially enter
-    chassis.arcade(90, 0);
-    pros::delay(350);
-    // slow cross
-    chassis.arcade(70, 0);
-    constexpr uint32_t cross_time = 3100;
-    constexpr uint32_t drop_time = 900;
-    pros::delay(drop_time);
-    set_matchloader(true);
-    pros::delay(cross_time - drop_time);
-
-    // correct heading
-    chassis.turnToHeading(180, 600, {}, false);
-    // back up
-    chassis.arcade(-60, 0);
-    pros::delay(800);
-    chassis.arcade(0, 0);
+    park_cross(true);
 
     // reset pose
     const float side_distance = find_dist(left_dist, left_dist_offset, M_PI, Dir::Left, 100);
@@ -168,24 +195,14 @@ void skills()
     intake_set_state(SCORE_MID);
     pros::delay(3500);
 
+    // reset pose at mid goal
+    chassis.setPose(14, -14, chassis.getPose().theta);
+
     // 4. long goal 2
 
     long_goals(false);
 
     // 5. park
 
-    // position to park zone
-    intake_set_state(IDLE);
-    chassis.moveToPose(-63.5, -24, -80, 1500, {.horizontalDrift = 8, .minSpeed = 60, .earlyExitRange = 2}, false);
-    chassis.arcade(60, 0);
-    pros::delay(1000);
-
-    // enter park zone
-    intake_set_state(IDLE);
-    // sharp to initially enter
-    chassis.arcade(90, 0);
-    pros::delay(300);
-    // slow cross
-    chassis.arcade(60, 0);
-    pros::delay(700);
+    park_cross(false);
 }
